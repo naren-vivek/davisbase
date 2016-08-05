4aeaa49a650ad9f5a7d3a1aca4f7625dc250a42a
Sandeep Ganesan <sandeep.ganesan@gmail.com>
2016-08-04 22:53:50 -0500

    Leaf cell first commit

---
+++ src/com/utdallas/db/davisbase/file/LeafCell.java
@@ -0,0 +1,24 @@
+package com.utdallas.db.davisbase.file;
+
+import java.io.IOException;
+
+public class LeafCell {
+
+	short totalPayload;
+	int rowId;
+	byte noOfColumns;
+	byte[] colTypes;
+	byte[][] columns;
+
+	public void writeCell(TableFile file, long offset) throws IOException {
+		file.seek(offset);
+		file.writeShort(totalPayload);
+		file.writeInt(rowId);
+		file.writeByte(noOfColumns);
+		file.write(colTypes);
+		for (byte[] column : columns) {
+			file.write(column);
+		}
+	}
+
+}
